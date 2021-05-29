0c740681c11e2e711d9c0625f60cafb2c39df29c
Bear <648070256@qq.com>
2021-05-29 17:07:03 +0800

    继续开发

--- Example/XRTaskManager/MyPod/XRTask.h
+++ Example/XRTaskManager/MyPod/XRTask.h
@@ -28,8 +28,7 @@ typedef NS_ENUM(NSInteger, XRTaskStatus) {
 
 @interface XRTask : NSObject
 
-
-#pragma mark - 配置型参数
+#pragma mark 配置型参数
 /// 只在XRTaskSchedulerTypePriority类型的任务中生效（默认：XRTaskPriorityDefault）
 @property (nonatomic, assign) XRTaskPriority priority;
 /// 任务唯一ID
@@ -39,8 +38,7 @@ typedef NS_ENUM(NSInteger, XRTaskStatus) {
 /// 任务完成后，task是否需要缓存（默认：NO）
 @property (nonatomic, assign) BOOL ifNeedCacheWhenCompleted;
 
-
-#pragma mark - Block型参数
+#pragma mark Block型参数
 /**
  * task任务
  * block：设置型
@@ -58,26 +56,28 @@ typedef NS_ENUM(NSInteger, XRTaskStatus) {
  */
 @property (nonatomic, copy) XRParseIsComplete parseIsComplete;
 
-
-#pragma mark - 只读型参数
+#pragma mark 只读型参数
 /// 任务状态（默认：Idle）
 @property (nonatomic, assign, readonly) XRTaskStatus taskStatus;
 /// completeBlock生成的返回数据
 @property (nonatomic, strong, readonly) id responseData;
 
-#pragma mark - 其他无关参数
+#pragma mark 其他无关参数
 /// 自定义数据（调用方可以把一些自定义信息存在这里）
 @property (nonatomic, strong) id customData;
 ///  task创建时间
 @property (nonatomic, strong, readonly) NSString *createDate;
 
 
-#pragma mark - Public
+#pragma mark - Public Func
+#pragma mark task完成时，执行的任务
 /// 在任务完成时尝试执行block
 /// @param taskBlock 任务block
 - (void)tryToExecuteCompletedTaskBlock:(XRTaskBlock)taskBlock;
 /// 在任务完成时尝试执行taskScheduler
 - (void)tryToExecuteCompletedScheduler;
+
+#pragma mark 执行/取消task
 /// 执行任务
 - (void)executeTask;
 /// 取消任务
